de6e8334ee8351ce07a3d9bd67e81109c2d0b131
ShivanshDahiya5 <shivanshdahiya563@gmail.com>
2026-09-06 09:20:25 +0530

    Add save/remove button with icon to MovieDetails component

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -197,3 +197,8 @@ function MovieDetails({ id, isFavorite, addFavorite, removeFavorite }) {
                   : 'bg-gradient-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700 text-white shadow-lg hover:shadow-purple-500/10'
               }`}
             >
+              <svg className="w-4.5 h-4.5 fill-current" viewBox="0 0 20 20">
+                <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
+              </svg>
+              <span>{favorited ? 'Remove from Library' : 'Save to Library'}</span>
+            </button>
