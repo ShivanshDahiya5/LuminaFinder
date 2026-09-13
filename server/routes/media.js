4e88e7823f3c240b8c5a136ca0757fe42ac3b109
ShivanshDahiya5 <shivanshdahiya563@gmail.com>
2026-09-13 11:06:38 +0530

    Add initial media routes with external API constants

---
+++ server/routes/media.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+const TVMAZE_BASE = 'https://api.tvmaze.com';
+const OPEN_LIBRARY_BASE = 'https://openlibrary.org';
+const GOOGLE_BOOKS_BASE = 'https://www.googleapis.com/books/v1/volumes';
+const ITUNES_BASE = 'https://itunes.apple.com/search';
+const WIKI_SEARCH_BASE = 'https://en.wikipedia.org/w/api.php';
+const WIKI_SUMMARY_BASE = 'https://en.wikipedia.org/api/rest_v1/page/summary';
